0f190fb4564b423b63e65c6e70515c4eca8e66a3
meghanaraokl <meghanaraokl@gmail.com>
2020-04-09 18:42:57 +0530

    commit

--- src/com/meghana/ArrayOfNumbers.java
+++ src/com/meghana/ArrayOfNumbers.java
@@ -3,18 +3,32 @@ package com.meghana;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class ArrayOfNumbers {
+public class ArrayOfNumbers { 
 
 	public static void main(String[] args) {
 
-		ArrayList<Number> NUM = new ArrayList<Number>();
-		int values = GiveArrayOfNumbers();
-		NUM.add(values);
+		ArrayList<Number> numbers = new ArrayList<Number>();
+		numbers.add(10);
+		numbers.add(20);
+		numbers.add(30);
+		numbers.add(40);
+		numbers.add(50);
+				
+	int a=readIntergerKey();
+	if(numbers.contains(a))
+	{
+	  System.out.println("The number "+a+ " is present");	
+}
+	else
+	{
+		System.out.println("The number "+a+ " is not present");
+	}
+	
 	}
 
-	public static int GiveArrayOfNumbers() {
+	public static int readIntergerKey() {
 		Scanner s = new Scanner(System.in);
-		System.out.println("Enter number of  students");
+		System.out.println("Enter the value");
 		int Array = s.nextInt();
 		return Array;
 	}
--- src/com/meghana/Student.java
+++ src/com/meghana/Student.java
@@ -1,28 +1,20 @@
 package com.meghana;
 
+import java.util.Scanner;
 
-	
-	import java.util.Scanner;
-	public class Student {
-		int Student_id;
-		String Student_name;
+public class Student {
+	int Student_id;
+	String Student_name;
 
-		
-		public  int readStudentId() {
-			Scanner s1 = new Scanner(System.in);
-		    System.out.println("Enter id of the student ");
-		    Student_id = s1.nextInt();
-			
-		    return Student_id;
-		
-		}
-		    
-		   public  void readStudentName() {
-		    Scanner s2 = new Scanner(System.in);
-		    System.out.println("Enter name of the student ");
-		    Student_name= s2.nextLine();
-			}
+	public void readStudentId() {
+		Scanner s1 = new Scanner(System.in);
+		System.out.println("Enter id of the student ");
+		 Student_id = s1.nextInt();
 	}
 
-
+	public void readStudentName() {
+		Scanner s2 = new Scanner(System.in);
+		System.out.println("Enter name of the student ");
+		 Student_name = s2.nextLine();
+	}
 }

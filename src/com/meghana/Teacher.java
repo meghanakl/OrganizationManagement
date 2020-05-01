dcf40faab17a4d243f012a8dd704d2b18a10b556
meghanaraokl <meghanaraokl@gmail.com>
2020-05-01 12:56:35 +0530

    commit

--- src/com/meghana/Teacher.java
+++ src/com/meghana/Teacher.java
@@ -3,6 +3,14 @@ package com.meghana;
 public class Teacher extends CollegeStaff {
 	String Teaching_Subject;
 	int Teaching_hours;
+	
+	public double IncrementSalary()
+	{
+		 Staff_Salary=(Staff_Salary)+(Staff_Salary)*(0.2);
+		 return Staff_Salary;
+	}
+	
+	
 
 }
  
